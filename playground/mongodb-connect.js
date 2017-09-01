39dd13792fa3df31ac0c9981cb4a2b6e00d95e03
Scott Smith <scott.smith1500@gmail.com>
2017-09-01 13:29:16 -0400

    =Added Update script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -8,30 +8,30 @@ MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, db) => {
 
   console.log('Connected to MongoDB server');
 
-  db.collection('Todos').insertOne({
-    text: 'Something to do',
-    completed: false
-  }, (err, result) => {
-    if (err) {
-        return console.log('Unable to insert todo: ', err);
-    }
-
-    console.log(JSON.stringify(result.ops, null, 2));
-  });
-
-  db.collection('Users').insertOne({
-    name: 'Scott Smith',
-    age: 52,
-    location: 'Clover'
-  }, (err, result) => {
-    if (err) {
-        return console.log('Unable to insert user: ', err);
-    }
-
-    // console.log(JSON.stringify(result.ops, null, 2));
-
-    console.log(result.ops[0]._id.getTimestamp());
-  });
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if (err) {
+  //       return console.log('Unable to insert todo: ', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, null, 2));
+  // });
+  //
+  // db.collection('Users').insertOne({
+  //   name: 'Scott Smith',
+  //   age: 52,
+  //   location: 'Clover'
+  // }, (err, result) => {
+  //   if (err) {
+  //       return console.log('Unable to insert user: ', err);
+  //   }
+  //
+  //   // console.log(JSON.stringify(result.ops, null, 2));
+  //
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
   db.close();
 });
---
+++ playground/mongodb-update.js
@@ -0,0 +1,31 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+
+  console.log('Connected to MongoDB server');
+
+  // findOneAndUpdate
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('59a971b0f208c019584d890f')
+  }, {
+    $set: {
+      name: 'Brandon'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+
+  // 59a984d30b6c53bb29933fb4
+
+  //db.close();
+});
