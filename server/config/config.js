be7a9d31a636491fd912de38c5b86ba387bb5bae
Scott Smith <scott.smith1500@gmail.com>
2017-09-06 15:35:31 -0400

    =Set up for Mongo on AWS EC2

--- server/config/config.js
+++ server/config/config.js
@@ -2,8 +2,10 @@ var env = process.env.NODE_ENV || 'development';
 
 if (env ==='development') {
   process.env.port = 3000
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoApp'
+  //process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoApp'
+  process.env.MONGODB_URI = 'mongodb://54.83.92.56:27017/ToDoApp'
 } else if (env === 'test') {
   process.env.port = 3000
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoAppTest'
+  //process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoAppTest'
+  process.env.MONGODB_URI = 'mongodb://54.83.92.56:27017/ToDoAppTest'
 }
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
+var uri = 'mongodb://54.83.92.56:27017/ToDoApp';
+
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+//mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(uri);
 
 module.exports = {mongoose};
