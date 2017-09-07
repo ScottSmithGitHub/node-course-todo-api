c24b22378cf77873ca012622157bdde96f42689d
Scott Smith <scott.smith1500@gmail.com>
2017-09-07 08:31:45 -0400

    =changed to new aws mongo server

--- server/config/config.js
+++ server/config/config.js
@@ -3,9 +3,9 @@ var env = process.env.NODE_ENV || 'development';
 if (env ==='development') {
   process.env.port = 3000
   //process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoApp'
-  process.env.MONGODB_URI = 'mongodb://54.83.92.56:27017/ToDoApp'
+  process.env.MONGODB_URI = 'mongodb://54.175.27.103:27017/ToDoAppDev'
 } else if (env === 'test') {
   process.env.port = 3000
   //process.env.MONGODB_URI = 'mongodb://localhost:27017/ToDoAppTest'
-  process.env.MONGODB_URI = 'mongodb://54.83.92.56:27017/ToDoAppTest'
+  process.env.MONGODB_URI = 'mongodb://54.175.27.103:27017/ToDoAppTest'
 }
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-var uri = 'mongodb://54.83.92.56:27017/ToDoApp';
+var uri = 'mongodb://54.175.27.103:27017/ToDoApp';
 
 mongoose.Promise = global.Promise;
 //mongoose.connect(process.env.MONGODB_URI);
